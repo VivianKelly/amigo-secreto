d5828ea6d92430552889b7ee8e0d4208d862f0b6
Vivian Kelly <winterplanwt@gmail.com>
2025-01-25 14:40:22 -0300

    Implementando função para agregar amigos e função para limpar o campo

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 let listaDeAmigos = [];
+
+function adicionarAmigo(){
+    let nomeAmigo = document.querySelector('input').value;
+    if(nomeAmigo == ''){
+        alert('Por favor, insira um nome válido.');
+    }
+    else{
+        listaDeAmigos.push(nomeAmigo);
+        limpaCampo();
+    }
+}
+
+function limpaCampo(){
+    nomeAmigo = document.querySelector('input');
+    nomeAmigo.value = '';
+}
